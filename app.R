cc1a739e79900fadbf4b1737132ec1e66680570e
rhidmoore <rmoore84@vols.utk.edu>
2019-10-30 11:38:43 -0400

    Added description and loaded libraries

--- app.R
+++ app.R
@@ -1,49 +1,13 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+########
+# Shiny app to create a dynamically-filterable visualization of the diamonds app
+########
 
+# These bits get run before any of the rest of the code
+# Note: contrary to what I told you on Monday, the use of the global.R file is no longer recommended.
+# At present, I'm not sure why.
 library(shiny)
+library(tidyverse)
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
-    )
-)
-
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    })
-}
 
 # Run the application 
 shinyApp(ui = ui, server = server)
